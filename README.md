d0b04e6e85ee5245b6011eb9fab454012c40aeb0
Vương <vuong.le@onemount.com>
2022-01-17 12:14:30 +0700

    feat(branch): checkout last active branch

---
+++ README.md
@@ -0,0 +1,41 @@
+# Git Statistics
+
+- In the end of the year, i am assigned to a task to collect statistics of coding:
+    - Number of active repos
+    - Number of code lines
+    - Number of commits
+
+- There are a lot of repos in my company and there is no tool in the market satisfied the requirements so I have to write a tool in bash script by myself
+
+- Some following notes how to collect statistics and work with bash shell:
+
+## Git commits between two dates
+```
+git log <branch-name> --pretty="%h %an %ad" --since=2021-01-01 --until=2022-01-14
+
+git log <branch-name> --pretty="%h" --since=2021-01-01
+```
+
+## The last commit
+```
+git log <branch-name> -n1 --format="%h"
+```
+
+## The first commit since a date
+```
+git log <branch-name> --pretty="%h" --since=2021-01-01 | tail -1
+```
+
+
+## Show inserted/deleted lines of code betweeen 2 commits
+```
+git diff --shortstat <c1> <c2>
+```
+
+## Show number of commits between 2 commits
+```
+git log <c1>..<c2> --pretty=oneline | wc -l
+
+git log <branch-name> --pretty="%h" --since=2021-01-01 | wc -l
+```
+
---
+++ git-stats.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+# Author: VKing34
+
+
+### global vars
+repos=(
+    ""
+)   
+since="2021-01-01"
+totalRepos=0
+totalCommits=0
+totalCodeLines=0
+
+
+### funcs
+function cloneRepos() {
+    echo "Starting to clone repos..."
+    for repo in ${repos[@]}; do
+        echo "Starting to clone: $repo"
+        git clone $repo
+    done
+}
+
+function collectStatisticsFromAll() {
+echo "Starting to collect statistics..."
+for repoDir in */ ; do
+    cd $repoDir
+    echo "=============================================="
+    echo "Repo: $repoDir"
+    collectStatisticsFromOne
+    let "totalRepos += 1"
+    cd ..
+done
+}
+
+function collectStatisticsFromOne() {
+    checkoutBranch
+    collectStatistics
+}
+
+function collectStatistics() {
+    # code lines
+    firstCommit=$(git log --pretty="%h" --since=$since | tail -1)
+    echo "First commit: $firstCommit"
+    # lastCommitInLastYear=$(git log --format="%h" -n 1 ${firstCommit}~)
+    codeLines=$(git diff --shortstat $firstCommit HEAD | tr '\n' ' ' | sed -e 's/[^0-9]/ /g' -e 's/^ *//g' -e 's/ *$//g' | tr -s ' ' | sed 's/ /\n/g' | sed -n 2p)
+    echo "Number of code lines: $codeLines"
+    let "totalCodeLines += $codeLines"
+
+    # commits
+    commitNo=$(git log --pretty="%h" --since=$since | wc -l)
+    echo "Number of commits: $commitNo"
+    let "totalCommits += $commitNo"
+}
+
+function checkoutBranch() {
+    prefix="origin/"
+    remoteTargetBranch=$(git branch -r --sort=-committerdate | head -n 1 | xargs)
+    targetBranch=${remoteTargetBranch#"$prefix"}
+    git checkout $targetBranch
+    echo "Checked out to: $targetBranch"
+
+    # targetBranchs=(
+    #     "qc"
+    #     "uat"
+    #     "uat-cloud"
+    #     "release.dfs"
+    #     "release"
+    #     "develope"
+    #     "master"
+    # )
+    # targetBranch=""
+    # branches=$(git branch -r)
+    # # echo "$branches"
+    # for branch in ${targetBranchs[@]}; do
+    #     if grep -q "origin/$branch" <<< $branches ; then
+    #         targetBranch=$branch
+    #         break
+    #     fi
+    # done
+
+    # # echo "branch: $targetBranch"
+    # if [ -z "$targetBranch" ]
+    # then
+    #     echo "!!! No target branch"
+    # else
+    #     git checkout $targetBranch
+    #     echo "Checked out to: $targetBranch"
+    # fi
+}
+
+function showStatistics() {
+    echo "===================RESULT======================="
+    echo "Total number of repos: $totalRepos"
+    echo "Total number of code lines: $totalCodeLines"
+    echo "Total number of commits: $totalCommits"
+}
+
+
+### main
+cloneRepos
+collectStatisticsFromAll
+showStatistics
